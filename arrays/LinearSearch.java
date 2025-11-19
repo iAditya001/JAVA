d2241fed16ba3f7c9710b85660a54bb27b72384c
Aditya Kumar Yadav <adityajr.asr07@gmail.com>
2025-11-20 00:36:40 +0530

    "19"

--- arrays/LinearSearch.java
+++ arrays/LinearSearch.java
@@ -2,7 +2,7 @@ package arrays;
 
 public class LinearSearch {
     public static int linearSearch(int numbers[],int key){
-        for(int i=0;i<=numbers.length;i++){
+        for(int i=0;i<numbers.length;i++){
             if(numbers[i]==key){
                 return i;
             }
