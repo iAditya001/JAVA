4e1e85734c1ffae4ef874e1bcb2aa8d86b4efca3
Aditya Kumar Yadav <adityajr.asr07@gmail.com>
2025-12-01 11:39:06 +0530

    Add pairs_array program

---
+++ arrays/pairs_array.java
@@ -0,0 +1,21 @@
+package arrays;
+
+public class pairs_array {
+    public static void Printpairs(int numbers[]){
+        int tp=0;
+       for(int i=0;i<numbers.length;i++){
+        int curr=numbers[i];
+        for(int j=i+1;j<numbers.length;j++){
+            System.out.print("("+curr+","+numbers[j]+")");
+            tp++;
+             }
+             System.out.println();
+            }
+            System.out.print("Total pairs "+tp+".       "  ) ;
+              }
+    public static void main(String[] args) {
+        int numbers[]={2,4,6,8,10};
+        Printpairs(numbers);
+    }
+    
+}
